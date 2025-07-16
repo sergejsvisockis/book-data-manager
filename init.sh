5e7cd1d96ea423ec28dbbcfb99e5a32ef076dc20
Sergejs Visockis <sergei.visotsky@gmail.com>
2025-07-16 23:19:52 +0200

    clean up init file

--- init.sh
+++ init.sh
@@ -6,10 +6,4 @@ pushd flink-connector-aws
   mvn clean install -Dmaven.test.skip=true -T1C
 popd
 
-git clone git@github.com:sergejsvisockis/book-manager.git
-
-pushd book-manager
-  mvn clean install -DskipTests -T1C
-popd
-
 mvn -U clean install
